8a8e1678dd41fa021b34e49b5af92497ce9a0e1a
Balsam-Faysal <blsam.2332016@gmail.com>
2018-03-27 15:28:05 +0300

    first stracture

---
+++ frontEnd/logic.js
@@ -0,0 +1,32 @@
+//function return the title of recipes from the api
+function recipTitle(response) {
+  var titleArr = [];
+  for (let i of response.results) {
+    titleArr.push(i.title);
+  }
+  return titleArr;
+};
+
+//function return the ingredients of recipes from the api
+function recipIngredients(response) {
+  var ingredientsArr = [];
+  for (let i of response.results) {
+    ingredientsArr.push(i.ingredients);
+  }
+  return ingredientsArr;
+};
+
+function fetch(url, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      var response = JSON.parse(xhr.responseText);
+      callback(response);
+    }
+  }
+  xhr.open('GET', url)
+  xhr.send();
+};
+
+
+module.exports = [recipTitle , recipIngredients ];
